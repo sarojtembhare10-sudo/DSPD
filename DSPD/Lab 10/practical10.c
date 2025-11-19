7fdfb14d1db19708a021fc7329ac8f0cdb651f21
Saroj9344 <sarojtembhare10@gmail.com>
2025-11-19 09:52:29 +0530

    Update practical10.c

--- DSPD/Lab 10/practical10.c
+++ DSPD/Lab 10/practical10.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
     FILE *src, *dest;
-    char sourceFile[50], destFile[50];
     char ch;
 
-    printf("Enter source file name: ");
-    scanf("%s", sourceFile);
-    printf("Enter destination file name: ");
-    scanf("%s", destFile);
+    src = fopen("input.txt", "r");
 
-    src = fopen(sourceFile, "r");
-    if (src == NULL) {
-        printf("Error: Cannot open source file.\n");
-        exit(1);
-    }
+    dest = fopen("output.txt", "w");
 
-    dest = fopen(destFile, "w");
-    if (dest == NULL) {
-        printf("Error: Cannot create destination file.\n");
-        fclose(src);
-        exit(1);
+    if (src == NULL || dest == NULL) {
+        printf("Error opening file.\n");
+        return 1;
     }
 
     while ((ch = fgetc(src)) != EOF) {
         fputc(ch, dest);
     }
 
-    printf("File copied successfully from %s to %s\n", sourceFile, destFile);
+    printf("File copied successfully.\n");
 
     fclose(src);
     fclose(dest);
