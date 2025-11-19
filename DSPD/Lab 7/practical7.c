31a9356e189c6c56768210bf3f56fb11ed300a21
Saroj9344 <sarojtembhare10@gmail.com>
2025-11-19 09:50:20 +0530

    Update practical7.c

--- DSPD/Lab 7/practical7.c
+++ DSPD/Lab 7/practical7.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
+
 #define MAX 10
-int AM[MAX][MAX];
-int n;
+int AM[MAX][MAX], n;
+
 void DFS() {
-    int stack[MAX], top = -1,visited[MAX], i, k;
+    int stack[MAX], top = -1, visited[MAX];
+    int i, k, start;
+
     for (i = 0; i < n; i++)
         visited[i] = 0;
 
-    printf("enter start node: ", n - 1);
-    scanf("%d", &k);
+    printf("Enter start node for DFS: ");
+    scanf("%d", &start);
+    stack[++top] = start;
+    visited[start] = 1;
 
-    stack[++top] = k;
-    visited[k] = 1;
-
-    printf("DFS order: ");
+    printf("DFS Traversal: ");
     while (top >= 0) {
         k = stack[top--];
         printf("%d ", k);
-
-        for (i = 0; i < n; i++) {
-            if (AM[k][i] && !visited[i]) {
+        for (i = n - 1; i >= 0; i--) {
+            if (AM[k][i] == 1 && visited[i] == 0) {
                 stack[++top] = i;
                 visited[i] = 1;
             }
@@ -27,64 +28,44 @@ void DFS() {
     }
     printf("\n");
 }
+
 void BFS() {
-    int q[MAX], front = 0, rear = 0;
-    int visited[MAX], i, k;
+    int q[MAX], front = -1, rear = -1, visited[MAX];
+    int i, k, start;
+
     for (i = 0; i < n; i++)
         visited[i] = 0;
-    printf("\nenter start node: ", n - 1);
-    scanf("%d", &k);
-    q[rear++] = k;
-    visited[k] = 1;
-    printf("BFS order: ");
-    while (front < rear) {
-        k = q[front++];
+
+    printf("Enter start node for BFS: ");
+    scanf("%d", &start);
+    q[++rear] = start;
+    visited[start] = 1;
+
+    printf("BFS Traversal: ");
+    while (front != rear) {
+        k = q[++front];
         printf("%d ", k);
         for (i = 0; i < n; i++) {
-            if (AM[k][i] && !visited[i]) {
-                q[rear++] = i;
+            if (AM[k][i] == 1 && visited[i] == 0) {
+                q[++rear] = i;
                 visited[i] = 1;
             }
         }
     }
     printf("\n");
 }
+
 int main() {
-    int i, j, choice;
-    printf("enter number of nodes : ", MAX);
+    int i, j;
+    printf("Enter number of vertices: ");
     scanf("%d", &n);
-    printf("enter adjacency matrix :\n", n, n);
-    for (i = 0; i < n; i++)
-        for (j = 0; j < n; j++)
+    printf("Enter adjacency matrix (%d x %d):\n", n, n);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             scanf("%d", &AM[i][j]);
-    do {
-        printf("\nMENU\n");
-        printf("1. display adjacency matrix\n");
-        printf("2. DFS order:\n");
-        printf("3. BFS order\n");
-        printf("4. exit\n");
-        printf("enter your choice: ");
-        scanf("%d", &choice);
-        switch (choice) {
-            case 1:
-                for (i = 0; i < n; i++) {
-                    for (j = 0; j < n; j++)
-                        printf("%d ", AM[i][j]);
-                    printf("\n");
-                }
-                break;
-            case 2:
-                DFS();
-                break;
-            case 3:
-                BFS();
-                break;
-            case 4:
-                printf("exiting\n");
-                break;
-            default:
-                printf("enter a valid choice\n");
         }
-    } while (choice != 4);
+    }
+    DFS();
+    BFS();
     return 0;
 }
